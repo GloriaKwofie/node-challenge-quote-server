44086a53b107ed0fa5772a5fdbb11f04f6dd8969
GloriaKwofie <91749390+GloriaKwofie@users.noreply.github.com>
2022-06-04 18:24:33 +0200

    the node exercise for week one

--- server.js
+++ server.js
@@ -17,7 +17,29 @@ app.get("/", function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
+
 //START OF YOUR CODE...
+
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
+
+app.get("/quotes7search", function (request, response) {
+  let term = request.query.term
+  if (term) {
+    term = term.toLowerCase()
+  }
+  console.log(term);
+  const filteredQuotes = quotes.filter(element => {
+    return element.quote.includes(term) || element.author.includes(term)
+  })
+  console.log(filteredQuotes);
+  response.send(quotes)
+});
 
 //...END OF YOUR CODE
 
